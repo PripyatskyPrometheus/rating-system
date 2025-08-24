dd6797fc38d150e654a76182ecfc39d31c7d73fd
Chelaev Petr <petrcelaev@gmail.com>
2025-08-24 21:26:53 +0400

    .

--- src/test/java/com/restaurant/ratingsystem/RestaurantRatingSystemApplicationTests.java
+++ src/test/java/com/restaurant/ratingsystem/RestaurantRatingSystemApplicationTests.java
@@ -3,17 +3,16 @@ package com.restaurant.ratingsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@TestPropertySource(properties = {
-    "spring.main.web-application-type=none",
-    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class RestaurantRatingSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
